972ad466e6c95aad9f3841231579506042e60b99
ldtuyen <ductuyenlieu@yahoo.com>
2017-03-26 23:59:00 +0700

    Initial commit

---
+++ src/test/java/org/tuyenlieu/simpletalk/SimpletalkApplicationTests.java
@@ -0,0 +1,16 @@
+package org.tuyenlieu.simpletalk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpletalkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
